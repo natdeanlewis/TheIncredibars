3c6075108e2e0592f13afdd03cbe5afd135b5d88
natdeanlewis <nat.deanlewis@gmail.com>
2021-03-20 19:51:41 +0000

    Added valid login audio to menu page

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+var fromPassword="false";
+
 function validateForm() {
     var x = document.forms["password-form"]["password"].value;
     
     if (x.toLowerCase()=="kronos") {
         changeTo("white");
+        fromPassword=true;
     } else {
         colors = ["red", "black"];
         // for (i = 0; i <= 5; i++) {
@@ -22,25 +25,9 @@ function validateForm() {
                 changeTo(colors[1]);
             }, gap * (1 + 2 * i));
         }
-
-        // setTimeout(function() {
-        //     changeTo("black");
-        // },300);
-        // setTimeout(function() {
-        //     changeTo("red");
-        // },600);
-        // setTimeout(function() {
-        //     changeTo("black");
-        // },900);
-        // setTimeout(function() {
-        //     changeTo("red");
-        // },1200);
-        // setTimeout(function() {
-        //     changeTo("black");
-        // },1500);
     }
     return (x.toLowerCase()=="kronos");
-  }
+}
 
 function changeTo(c) {
     document.getElementById("password").style.color = c;
@@ -56,7 +43,6 @@ function toggle() {
 }
 
 function mouseOver(c) {
-    console.log(c);
     var all = document.getElementsByClassName(c);
     for (var i = 0; i < all.length; i++) {
         all[i].style.color = "white";
@@ -66,7 +52,6 @@ function mouseOver(c) {
 }
 
 function mouseOut(c) {
-    console.log(c);
     var all = document.getElementsByClassName(c);
     for (var i = 0; i < all.length; i++) {
         all[i].style.color = 'black';
